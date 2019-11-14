6cbe4506537ff154066a9de4d0267f7f02dc756e
danileao <dani.leao89@gmail.com>
2019-11-14 16:39:55 -0300

    URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://my-json-server.typicode.com/danileao/serverproducts/",
+  baseURL: "https://my-json-server.typicode.com/danileao/serverproducts/",
 });
 
 export default api;
